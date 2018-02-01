48a8aa69db3236ea1895a95c54c52a7977c90245
Jason <jwestonwong@gmail.com>
2018-02-01 05:55:04 -0500

    Merge & Fix
    Merged Jesus' additions to the game and fixed critical bug issues that would cause crashes in the build.

---
+++ Assets/Objects/ReversePlatform.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReversePlatform : MonoBehaviour {
+
+    private SpriteRenderer sprite;
+    private Collider2D coll;
+
+    public Sprite[] sprites;
+    public buttonPress button;
+
+    // Use this for initialization
+    void Start () {
+        sprite = GetComponent<SpriteRenderer>();
+        coll = GetComponent<Collider2D>();
+        sprite.sprite = (sprites[1]);
+        coll.isTrigger = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Switches between being visible and touchable and invisible and intangiable
+        if (button != null)
+        {
+            if (button.Pressed == false)
+            {
+                sprite.sprite = (sprites[1]);
+                coll.isTrigger = true;
+            }
+            else
+            {
+                sprite.sprite = (sprites[0]);
+                coll.isTrigger = false;
+            }
+        }
+    }
+}
